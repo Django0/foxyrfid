c1ff9e8e22fa7a812817018f92735aaff7f8a54e
tilak.lakshmana <tilak.lakshmana@alten.se>
2017-02-19 16:39:42 +0100

    Added functionality to test puncher and read formatted data

---
+++ code/testFillPuncherWithDummyFoxData/testFillPuncherWithDummyFoxData.ino
@@ -0,0 +1,326 @@
+/**************************************************************************/
+/*!
+    @file     readMifare.pde
+    @author   Adafruit Industries
+  @license  BSD (see license.txt)
+
+    This example will wait for any ISO14443A card or tag, and
+    depending on the size of the UID will attempt to read from it.
+
+    If the card has a 4-byte UID it is probably a Mifare
+    Classic card, and the following steps are taken:
+
+    - Authenticate block 4 (the first block of Sector 1) using
+      the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
+    - If authentication succeeds, we can then read any of the
+      4 blocks in that sector (though only block 4 is read here)
+
+    If the card has a 7-byte UID it is probably a Mifare
+    Ultralight card, and the 4 byte pages can be read directly.
+    Page 4 is read by default since this is the first 'general-
+    purpose' page on the tags.
+
+
+  This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
+  This library works with the Adafruit NFC breakout
+  ----> https://www.adafruit.com/products/364
+
+  Check out the links above for our tutorials and wiring diagrams
+  These chips use SPI or I2C to communicate.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+*/
+/**************************************************************************/
+#define PUNCHER_ID  (0x09)
+uint8_t indexToWriteFoxData = 0x07;
+// Test Data to write to puncher
+
+#define NUM_PUNCHED (6)
+#define DATA_FOX_HHMMSS (4)
+
+uint8_t testDataFromFox[NUM_PUNCHED][DATA_FOX_HHMMSS] = { {0x01, 0x00, 0x0A, 0x2C}, // FoxID, HH, MM, SS
+  {0x02, 0x00, 0x0D, 0x26},
+  {0x03, 0x00, 0x0F, 0x08},
+  {0x04, 0x00, 0x10, 0x39},
+  {0x7D, 0x22, 0xE2, 0x00}, // Don't know which fox did this,
+  {0x05, 0x01, 0x0A, 0x2C}
+};
+
+/**************************************************************************/
+#include <Wire.h>
+#include <RtcDS3231.h>
+RtcDS3231<TwoWire> Rtc(Wire);
+
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+
+// If using the breakout or shield with I2C, define just the pins connected
+// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
+#define PN532_IRQ   (2)
+#define PN532_RESET (3)  // Not connected by default on the NFC Shield
+
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
+#define BUZZ_PIN   (3)
+#define LED_PIN    (4)
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+void setupRTC(void) {
+  Serial.print("compiled: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+  //--------RTC SETUP ------------
+  Rtc.Begin();
+
+  // if you are using ESP-01 then uncomment the line below to reset the pins to
+  // the available pins for SDA, SCL
+  // Wire.begin(0, 2); // due to limited pins, use pin 0 and 2 for SDA, SCL
+
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  printDateTime(compiled);
+  Serial.println();
+
+  if (!Rtc.IsDateTimeValid())
+  {
+    // Common Cuases:
+    //    1) first time you ran and the device wasn't running yet
+    //    2) the battery on the device is low or even missing
+
+    Serial.println("RTC lost confidence in the DateTime!");
+
+    // following line sets the RTC to the date & time this sketch was compiled
+    // it will also reset the valid flag internally unless the Rtc device is
+    // having an issue
+
+    Rtc.SetDateTime(compiled);
+  }
+
+  if (!Rtc.GetIsRunning())
+  {
+    Serial.println("RTC was not actively running, starting now");
+    Rtc.SetIsRunning(true);
+  }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  if (now < compiled)
+  {
+    Serial.println("RTC is older than compile time!  (Updating DateTime)");
+    Rtc.SetDateTime(compiled);
+  }
+  else if (now > compiled)
+  {
+    Serial.println("RTC is newer than compile time. (this is expected)");
+  }
+  else if (now == compiled)
+  {
+    Serial.println("RTC is the same as compile time! (not expected but all is fine)");
+  }
+
+  // never assume the Rtc was last configured by you, so
+  // just clear them to your needed state
+  Rtc.Enable32kHzPin(false);
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
+}
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+void RTCReadOut() {
+  if (!Rtc.IsDateTimeValid())
+  {
+    // Common Cuases:
+    //    1) the battery on the device is low or even missing and the power line was disconnected
+    Serial.println("RTC lost confidence in the DateTime!");
+  }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  printDateTime(now);
+  Serial.println();
+
+  RtcTemperature temp = Rtc.GetTemperature();
+  Serial.print(temp.AsFloat());
+  Serial.println("C");
+}
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+void setup(void) {
+  Serial.begin(115200);
+  delay(1000);
+  // Initialize the RTC
+  setupRTC();
+
+  // Initialize the NFC
+  Serial.println("Hello!");
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
+
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+
+  Serial.println("Waiting for an ISO14443A Card ...");
+  pinMode(LED_PIN, OUTPUT);
+  ShowNotification();
+}
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+void loop(void) {
+  Serial.println("Waiting ...");
+  uint8_t keya[6] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+  uint8_t success;
+  uint8_t uid[16];  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success) {
+    // Display some basic information about the card
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
+    Serial.println("");
+
+    if (uidLength == 4)
+    {
+      // We probably have a Mifare Classic card ...
+      Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
+
+      // Now we need to try to authenticate it for read/write access
+      // Try with the factory default KeyA: 0xFF 0xFF 0xFF 0xFF 0xFF 0xFF
+      Serial.println("Trying to authenticate block 4 with default KEYA value");
+
+      // Start with block 4 (the first block of sector 1) since sector 0
+      // contains the manufacturer data and it's probably better just
+      // to leave it alone unless you know what you're doing
+      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya);
+
+      if (success)
+      {
+        Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
+        uint8_t data[16];
+
+        // If you want to write something to block 4 to test with, uncomment
+        // the following line and this text should be read back in a minute
+        //memcpy(data, (const uint8_t[]){ 'a', 'd', 'a', 'f', 'r', 'u', 'i', 't', '.', 'c', 'o', 'm', 0, 0, 0, 0 }, sizeof data);
+        // success = nfc.mifareclassic_WriteDataBlock (4, data);
+
+        // Try to read the contents of block 4
+        success = nfc.mifareclassic_ReadDataBlock(4, data);
+
+        if (success)
+        {
+          // Data seems to have been read ... spit it out
+          Serial.println("Reading Block 4:");
+          nfc.PrintHexChar(data, 16);
+          Serial.println("");
+
+          RTCReadOut();
+          // Wait a bit before reading the card again
+          delay(1000);
+        }
+        else
+        {
+          Serial.println("Ooops ... unable to read the requested block.  Try another key?");
+        }
+      }
+      else
+      {
+        Serial.println("Ooops ... authentication failed: Try another key?");
+      }
+    }
+
+    if (uidLength == 7)
+    {
+      // We probably have a Mifare Ultralight card ...
+      Serial.println("Seems to be a Mifare Ultralight tag (7 byte UID)");
+      Serial.println("Trying to authenticate block 4 with default KEYA value");
+      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4 + 0, 0, keya);
+
+      // Try to read the first general-purpose user page (#4)
+      Serial.println("Reading page 4");
+#define SIZE_OF_DATA  (4)
+      uint8_t data[SIZE_OF_DATA];
+      for (uint8_t iPage = 0; iPage <= NUM_PUNCHED; iPage++)
+      {
+        uint8_t writePuncherID[SIZE_OF_DATA];
+        memset(writePuncherID, 0x0, SIZE_OF_DATA * sizeof(uint8_t));
+        if (iPage == 0)
+        {
+          writePuncherID[0] = PUNCHER_ID;
+          writePuncherID[1] = indexToWriteFoxData;
+        } else {
+          memcpy(writePuncherID, testDataFromFox[iPage - 1], SIZE_OF_DATA * sizeof(uint8_t));
+        }
+
+        success = nfc.mifareultralight_WritePage (4 + iPage, writePuncherID);
+        if (!success)
+        {
+          Serial.println("Ooops ... unable to read the requested page!?");
+          break;
+        }
+      }
+      for (uint8_t iPage = 0; iPage <= NUM_PUNCHED; iPage++)
+      {
+        success = nfc.mifareultralight_ReadPage (4 + iPage, data);
+        if (success)
+        {
+          // Data seems to have been read ... spit it out
+          nfc.PrintHexChar(data, SIZE_OF_DATA);
+          if (iPage == NUM_PUNCHED)
+          {
+            // Successfully cleared factor set the puncher
+            ShowNotification();
+          }
+        } else        {
+          Serial.println("Ooops ... unable to read the requested page!?");
+          break;
+        }
+      }
+      RTCReadOut();
+      // Wait a bit before reading the card again
+      delay(1000);
+    }
+  }
+}
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+void ShowNotification(void)
+{
+  tone(BUZZ_PIN, 440);
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                    // Wait a bit before reading the card again
+  noTone(BUZZ_PIN);
+  digitalWrite(LED_PIN, LOW);
+}
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+void printDateTime(const RtcDateTime& dt)
+{
+  char datestring[20];
+
+  snprintf_P(datestring,
+             countof(datestring),
+             PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+             dt.Month(),
+             dt.Day(),
+             dt.Year(),
+             dt.Hour(),
+             dt.Minute(),
+             dt.Second() );
+  Serial.print(datestring);
+}
+
